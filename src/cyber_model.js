88002dde87b341ddda1b7dac7fe83d86c855dd23
msmaple-dev <n8riemer@gmail.com>
2024-04-22 22:13:22 -0400

    Added addAsset Function

--- src/cyber_model.js
+++ src/cyber_model.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-let cyber_db = new sqlite3.Database('../../db/cyber_db.db', sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database('../../db/cyber_db.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
         console.error(err.message);
     }
@@ -8,11 +8,19 @@ let cyber_db = new sqlite3.Database('../../db/cyber_db.db', sqlite3.OPEN_READWRI
 
 export function getAssets() {
     let rows = [];
-    cyber_db.all(`SELECT * FROM assets ORDER BY asset_id`, [], (err, queryRows) => {
+    db.all(`SELECT * FROM assets ORDER BY asset_id`, [], (err, queryRows) => {
         if (err) {
             throw err;
         }
         rows = queryRows;
     });
     return rows;
+}
+
+export function addAsset(data) {
+    db.run( `INSERT INTO assets(name, type, serial_number, operating_system, department) VALUES (?, ?, ?, ?, ?)`, [...data.split(",")], (err) => {
+        if (err) {
+            throw err;
+        }
+    })
 }
